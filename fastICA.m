1909eaf692efc36ff5554e1770d0c518ce3ae30a
bertooon <filipow.bartosz@gmail.com>
2019-03-30 18:42:42 +0100

    Wybielanie działa. ICA niezbyt :/

--- fastICA.m
+++ fastICA.m
@@ -1,3 +1,5 @@
+format compact
+format long
 % Czytanie z pliku audio
 
 [y1,Fs1] = audioread('mial_ogromne_szczescie.wav');
@@ -21,13 +23,13 @@ t3 = t3(1:end);
 
 % sound(y1,Fs1)
 
-figure(1)
-subplot(2,2,1);
-plot(t1,y1);
-subplot(2,2,2);
-plot(t2,y2);
-subplot(2,2,3);
-plot(t3,y3);
+% figure(1)
+% subplot(2,2,1);
+% plot(t1,y1);
+% subplot(2,2,2);
+% plot(t2,y2);
+% subplot(2,2,3);
+% plot(t3,y3);
 
 % Wyrównywanie sygna³ów, tak ¿eby trwa³y tyle samo czasu
 koniec = 243653;
@@ -44,13 +46,17 @@ y3 = y3(1:1:koniec);
 Y1 = 0.4*y1 + 0.9*y2;
 Y2 = 0.9*y1 + 0.2*y2;
 
-figure(2)
-subplot(2,1,1);
-plot(t1,Y1);
-subplot(2,1,2);
-plot(t1,Y2);
+% figure(2)
+% subplot(2,1,1);
+% plot(t1,Y1);
+% subplot(2,1,2);
+% plot(t1,Y2);
+% 
+% figure(3)
+% plot(Y1,Y2,'.');
 
 % sound(ym1,Fs1);
+% pause
 % sound(ym2,Fs1);
 
 % Centrowanie
@@ -58,24 +64,56 @@ N = 2; % Iloœæ mieszanin
 M = koniec;
 X = [Y1'; Y2'];
 
-figure(3)
-subplot(2,1,1);
-plot(t1,X(1,:));
-subplot(2,1,2);
-plot(t2,X(2,:));
-hold on
+% figure(4)
+% subplot(2,1,1);
+% plot(t1,X(1,:));
+% subplot(2,1,2);
+% plot(t2,X(2,:));
+% hold on
+mx  = mean(X') 
 for i=1:N
         X(i,:) = X(i,:) - (1/M)*sum(X(i,:));
 end
 
-figure(3)
-subplot(2,1,1);
-plot(t1,X(1,:));
-subplot(2,1,2);
-plot(t2,X(2,:));
-hold off
+% figure(4)
+% subplot(2,1,1);
+% plot(t1,X(1,:));
+% subplot(2,1,2);
+% plot(t2,X(2,:));
+% hold off
 
-% Prewhite
+% Wybielanie
+c   = cov(X')		 % covariance
+sq  = inv(sqrtm(c));        % inverse of square root
+mx  = mean(X')             % mean
+XX  = X-mx'*ones(1,koniec); % subtract the mean
+XX  = 2*sq*XX;              
+cov(XX')                 % the covariance is now a diagonal matrix
+% figure(5); 
+% plot(XX(1,:), XX(2,:), '.');
 
+% Inicjalizacja
+a = rand(1);
+w0 = [a,0];
+w0(2) = sqrt(1 - (w0(1)^2));
+w0 = w0';
+norm(w0)
 
+% Iteracja
+eps = 1e-15;
+wi_1 = w0;
+Z = [XX(1,:)',XX(2,:)']';
 
+while 1 
+    wi = (1/koniec)*sum(Z*(wi_1'*Z)'.^3) - 3*wi_1;
+    wi = wi/norm(wi);
+    display(wi)
+
+    if ((wi'*wi_1)>1-eps) &&  ((wi'*wi_1)<1+eps)
+       break; 
+    end
+    wi_1 = wi;
+end
+
+czyste1 = wi'*Z;
+% sound(czyste1,Fs1)
